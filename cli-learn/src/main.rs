232fdb02150b6791c80e59c0a3a8aad1ce93b434
Rozstone <wangsiteng123@gmail.com>
2022-08-14 21:24:02 +0800

    cli learn

---
+++ cli-learn/src/main.rs
@@ -0,0 +1,15 @@
+use clap::Parser;
+
+fn main() {
+    // Too Complicated
+    // struct Cli {
+    //     pattern: String,
+    //     path: std::path::PathBuf,
+    // }
+    // let pattern = std::env::args().nth(1).expect("no pattern given");
+    // let path = std::env::args().nth(2).expect("no path given");
+    // let args = Cli {
+    //     pattern,
+    //     path: std::path::PathBuf::from(path),
+    // };
+}
