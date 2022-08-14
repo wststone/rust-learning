805a8b5ec3ad17ff4d6056965162cd65c3799e8c
Rozstone <wangsiteng123@gmail.com>
2022-08-14 11:44:30 +0800

    variables and shadowing

---
+++ variables/src/main.rs
@@ -0,0 +1,14 @@
+const MAX_POINTS: i64 = 100_000;
+
+// shadow
+fn main() {
+    let mut x = 5;
+    println!("Value of x is {}", x);
+    x = 6;
+    println!("Now Value of x is {}", x);
+    let x = x * 2;
+    println!("Now Value of x is {}", x);
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("Spaces is {}", spaces);
+}
